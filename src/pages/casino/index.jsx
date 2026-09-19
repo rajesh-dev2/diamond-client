460eb9bc19c5481d737c23c057897a498b91681e
devTrailor <ritik.trilok.dev@gmail.com>
2026-09-19 23:26:02 +0530

    feat: add casino page dispatcher and application routes

--- src/pages/casino/index.jsx
+++ src/pages/casino/index.jsx
@@ -25,13 +25,15 @@ const CASINO_MAP = {
   card32eu: Card32B,
   lucky7: Lucky7,
   lucky7eu: Lucky7B,
-  ab20: Ab20,
-  ab: Ab20,
-  'andar-bahar': Ab20,
-  abj: Abj,
-  ab2: Abj,
-  'andar-bahar-2': Abj,
+  ab20: Abj,
+  abj20: Abj,
+  ab: Abj,
+  'andar-bahar': Abj,
+  abj: Ab20,
+  ab2: Ab20,
+  'andar-bahar-2': Ab20,
 }
+
 
 export default function CasinoPage() {
   const { gameId } = useParams()
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -44,9 +44,11 @@ export default function AppRoutes() {
           <Route path="/lucky7eu" element={<Navigate to="/casino/lucky7eu" replace />} />
           <Route path="/lucky7eu2" element={<Navigate to="/casino/lucky7eu2" replace />} />
           <Route path="/ab20" element={<Navigate to="/casino/ab20" replace />} />
+          <Route path="/abj20" element={<Navigate to="/casino/ab20" replace />} />
           <Route path="/abj" element={<Navigate to="/casino/abj" replace />} />
           <Route path="/andar-bahar" element={<Navigate to="/casino/ab20" replace />} />
           <Route path="/andar-bahar-2" element={<Navigate to="/casino/abj" replace />} />
+
           <Route path="/casino-list/*" element={<Home />} />
           <Route path="/live-casino-list/*" element={<Home />} />
           <Route path="/sports-book/*" element={<Home />} />
